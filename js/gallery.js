db93a2746ef077d6eec8d167a571d1a34410b2c5
Josh Ellis <joshuaellis1991@gmail.com>
2020-11-23 16:03:02 +0000

    preload image

--- js/gallery.js
+++ js/gallery.js
@@ -11,6 +11,12 @@ function activateGallery() {
   let description = galleryInfo.querySelector(".description");
   
   thumbnails.forEach(function(thumbnail) {
+    // //Preload large images.
+    let newImageSrc  = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+    
+    
     thumbnail.addEventListener("click", function() {
       // Set clicked image as main image.
       let newImageSrc = thumbnail.dataset.largeVersion;
